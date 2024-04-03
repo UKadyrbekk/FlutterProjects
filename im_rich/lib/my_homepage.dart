a0fbd99df753e1350390910494b99fb023c4bb45
pro2019 <pro2019@pro2019154s-MacBook-Pro.local>
2024-04-03 15:12:56 +0600

    papka

---
+++ im_rich/lib/my_homepage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white10,
+          title: Center(child: Text('Im Rich')),
+          elevation: 5,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text( 
+                            "I'm Rich",
+                            style: TextStyle( color: Colors. red,
+              fontSize: 48,
+              fontWeight: FontWeight.w400,
+              fontFamily: 'Sofia-Regular',
+                            ),
+                          ),
+            Image.asset('assets/images/almaz.png'),
+            ],
+          ),
+        ),
+        );
+  }
+}
